02dc069b3fda4ae4eab753803fa3664b41f4473d
mdewey123 <147895420+mdewey123@users.noreply.github.com>
2025-03-03 23:24:46 +0000

    setting up javascript for wheel of spelling game

--- fortune/static/fortune/script.js
+++ fortune/static/fortune/script.js
@@ -1,30 +1,41 @@
 addEventListener("DOMContentLoaded", function() {
     const gameBoard = document.getElementById("game-board");
-    const welcomePuzz = this.document.getElementById("game-board").dataset.puzzle;
+    const gamePieces = document.querySelectorAll('game-pieces div')
+    
+    const puzzPieces = gameBoard.querySelectorAll("div");
+    const welcomePuzz = document.getElementById("game-board").dataset.puzzle;
 
     function populate_board(sentence) {
-        const puzzPieces = gameBoard.querySelectorAll("div");
         const letters = sentence.split("");
-    
         //populate with letters for puzzle sentence
         puzzPieces.forEach((piece, index) => {
             
             const h4 = piece.querySelector('h4');
             if (h4) {
                 h4.textContent = index < letters.length ? letters[index] : "";
+                h4.style.color = "red"
             }
-            h4.style.color = "white"
+            
         });
     
     };
     
+    function make_guess() {
+        gamePieces.forEach((gamePiece) =>{
+            gamePiece.addEventListener("click", function() {
+                puzzPieces.forEach((piece) =>{
+                    wagh = piece.querySelector('h4')
+                    if (wagh && gamePiece.dataset.letter === wagh.textContent) {
+                       wagh.style.color = "black";
+                    }
+                });
+            });
+        });
+    };
+    
+
+
     populate_board(welcomePuzz)
+    make_guess()
 
 });
-
-
-
-
-function make_guess() {
-
-};
