c98691332a8ca6f670d3e75f9ab7ee75542b9624
mdewey123 <147895420+mdewey123@users.noreply.github.com>
2025-04-16 22:51:46 +0900

    s

--- fortune/static/fortune/seating.js
+++ fortune/static/fortune/seating.js
@@ -1,3 +1,9 @@
 addEventListener("DOMContentLoaded", function() {
 
 });
+
+function load_seats() {
+    /* generate a series of boxes iside classroom div then add logic to generate new rows acording to classroom ResizeObserverSize. 
+    in top have a "Class size of __" thing amd make capable of changing the number then adapting seating based on that.
+    */
+}
